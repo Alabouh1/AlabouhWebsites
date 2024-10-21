3f1adc8b687ff6bdcd74c115ab17d266432740da
Alabouh1 <80550487+Alabouh1@users.noreply.github.com>
2024-10-21 17:07:15 +0100

    Add files via upload

---
+++ Basics1Examples.py
@@ -0,0 +1,27 @@
+
+print("Message")
+
+
+variable = 123
+print(variable)
+
+
+usernumber = int(input("Enter a number"))
+
+
+result = usernumber * 2
+
+
+if result == 2:
+    print("The original number was 1")
+else:
+    print("The original number was not 1")
+    
+    
+    
+if result == 2:
+    print("The original number was 1")
+elif result == 4:
+    print("The original number was 2")
+else:
+    print("The original number was not 1 or 2")
